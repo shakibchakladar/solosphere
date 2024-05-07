a8502f47fb8de5a6101fd3208fcb062a4a0288ae
shakibchakladar <shakibchakladar@gmail.com>
2024-05-07 17:08:15 +0600

    tab added

--- src/components/TabCategories.jsx
+++ src/components/TabCategories.jsx
@@ -3,11 +3,19 @@ import "react-tabs/style/react-tabs.css";
 
 const TabCategories = () => {
   return (
-    <Tabs>
+   <div className="container px-10 py-6 mx-auto">
+    <div className="px-10 my-5 space-y-4 text-center">
+    <h2 className="text-5xl font-bold">Browse Our Job Category</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem eveniet nemo obcaecati vitae. Accusamus provident, dolorem veniam rem veritatis reprehenderit.</p>
+    </div>
+     <Tabs>
+      <div className="flex items-center justify-center">
       <TabList>
-        <Tab>Title 1</Tab>
-        <Tab>Title 2</Tab>
+        <Tab>Web</Tab>
+        <Tab>Graphics </Tab>
+        <Tab>Digital m</Tab>
       </TabList>
+      </div>
 
       <TabPanel>
         <h2>Any content 1</h2>
@@ -15,7 +23,11 @@ const TabCategories = () => {
       <TabPanel>
         <h2>Any content 2</h2>
       </TabPanel>
+      <TabPanel>
+        <h2>Any content 3</h2>
+      </TabPanel>
     </Tabs>
+   </div>
   );
 };
 
